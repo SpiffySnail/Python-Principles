81c07c48c1e8819a92b15ba36ba1ebac01f98da1
SpiffySnail <harrisonjones2015@gmail.com>
2021-12-15 14:17:13 +0000

    Comment Update to Adding and Removing Dots.py
    Added some comments to help with idea suggetions.

--- Adding and Removing Dots.py
+++ Adding and Removing Dots.py
@@ -1,12 +1,12 @@
 def add_dots(param):
     result = ""
-    b = ""
+    b = "" #Variable to trake the previous iteration 
     for a in param:
-        if b == "":   
-            result = a
+        if b == "": #Will check if first character in string hasn't been set yet   
+            result = a #therfore, won't add a dot add. add character to string "result"
             b = a
         else:
-            result += '.' + a
+            result += '.' + a #else there must be a character so just add a dot, add char and dot to string "result"
             b = a
     return(result)
 
